c557ad11ff1777aad01a52056612e1a5cfbdb538
Miadern <465029437@qq.com>
2022-07-15 17:54:06 +0800

    ..

---
+++ src/views/Layout/index.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    布局页面
+    <router-view></router-view>
+    <van-tabbar route>
+      <van-tabbar-item icon="home-o" to="/ ">
+        <template #icon>
+          <span class="iconfont icon-shouye"></span>
+          <span class="text">首页</span>
+        </template>
+      </van-tabbar-item>
+      <van-tabbar-item icon="home-o" to="/video">
+        <template #icon>
+          <span class="iconfont icon-shipin"></span>
+          <span class="text">视频</span>
+        </template>
+      </van-tabbar-item>
+      <van-tabbar-item icon="home-o" to="/qa">
+        <template #icon>
+          <span class="iconfont icon-wenda"></span>
+          <span class="text">问答</span>
+        </template>
+      </van-tabbar-item>
+      <van-tabbar-item icon="home-o" to="/profile">
+        <template #icon>
+          <span class="iconfont icon-wode"></span>
+          <span class="text">我的</span>
+        </template>
+      </van-tabbar-item>
+    </van-tabbar>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped lang="less">
+:deep(.van-tabbar-item__icon) {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+  height: 100%;
+  .iconfont {
+    font-size: 0.533rem;
+  }
+  .text {
+    font-size: 0.32rem;
+  }
+}
+</style>
